cf89733b71daf085e0ff1e1bc6cc26dea6c781b2
traviest <traviest@TravMacBook.local>
2016-03-18 16:45:45 -0400

    alts

---
+++ process/confirm_auth.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <script>
+  function changePage() {
+    window.location.assign('/home.html');
+  }
+  </script>
+</head>
+<body>
+  <h3>Verifying Auth</h3>
+  <?php
+
+  ?>
+</body>
+</html>
